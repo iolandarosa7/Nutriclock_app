cb6b2db2deff622d88411e011a3e27e7e1bfdd7f
iolanda.rosa <iolanda.rosa@voidsoftware.com>
2021-10-12 23:07:30 +0100

    adicionada label ao calendario no diario do sono

--- lib/screens/navigation/sleep/SleepCalendarFragment.dart
+++ lib/screens/navigation/sleep/SleepCalendarFragment.dart
@@ -67,38 +67,79 @@ class _SleepCalendarFragmentState extends State<SleepCalendarFragment> {
             Positioned.fill(
               child: Padding(
                 padding: EdgeInsets.only(bottom: 150),
-                child: SfDateRangePicker(
-                  view: DateRangePickerView.month,
-                  todayHighlightColor: Color(0xFF60B2A3),
-                  selectionColor: Color(0xFFA3E1CB),
-                  monthCellStyle: DateRangePickerMonthCellStyle(
-                    todayTextStyle: TextStyle(
-                      color: Color(0xFF60B2A3),
+                child: Column(
+                  children: [
+                    GestureDetector(
+                      onTap: _showRegisterModal,
+                      child: Card(
+                        color: Color(0xFFA3E1CB),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(50),
+                            bottomRight: Radius.circular(50),
+                          ),
+                        ),
+                        margin: EdgeInsets.only(right: 40, bottom: 20, top: 20),
+                        shadowColor: Color(0xFFA3E1CB),
+                        elevation: 10,
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                left: 16, bottom: 8, top: 8, right: 4),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Seleciona uma data abaixo no calendário:",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    decoration: TextDecoration.none,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                    blackoutDatesDecoration: BoxDecoration(
-                        color: const Color(0xFFDFDFDF),
-                        border: Border.all(
-                            color: const Color(0xFFDFDFDF), width: 1),
-                        shape: BoxShape.circle),
-                    blackoutDateTextStyle: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.lineThrough),
-                  ),
-                  maxDate: new DateTime.now(),
-                  minDate: new DateTime.now().subtract(new Duration(days: 2)),
-                  onSelectionChanged:
-                      (DateRangePickerSelectionChangedArgs args) {
-                    final dynamic value = args.value;
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SleepRegisterFragment(
-                                value: value,
-                              )),
-                    ).then((value) => {_loadData()});
-                  },
-                  monthViewSettings:
-                      DateRangePickerMonthViewSettings(blackoutDates: _dates),
+                    SfDateRangePicker(
+                      view: DateRangePickerView.month,
+                      todayHighlightColor: Color(0xFF60B2A3),
+                      selectionColor: Color(0xFFA3E1CB),
+                      monthCellStyle: DateRangePickerMonthCellStyle(
+                        todayTextStyle: TextStyle(
+                          color: Color(0xFF60B2A3),
+                        ),
+                        blackoutDatesDecoration: BoxDecoration(
+                            color: const Color(0xFFDFDFDF),
+                            border: Border.all(
+                                color: const Color(0xFFDFDFDF), width: 1),
+                            shape: BoxShape.circle),
+                        blackoutDateTextStyle: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.lineThrough),
+                      ),
+                      maxDate: new DateTime.now(),
+                      minDate:
+                          new DateTime.now().subtract(new Duration(days: 2)),
+                      onSelectionChanged:
+                          (DateRangePickerSelectionChangedArgs args) {
+                        final dynamic value = args.value;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SleepRegisterFragment(
+                                    value: value,
+                                  )),
+                        ).then((value) => {_loadData()});
+                      },
+                      monthViewSettings: DateRangePickerMonthViewSettings(
+                          blackoutDates: _dates),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -106,5 +147,34 @@ class _SleepCalendarFragmentState extends State<SleepCalendarFragment> {
         ),
       ),
     );
+  }
+
+  Future<void> _showRegisterModal() async {
+    return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(
+              "Por favor seleciona uma data no calendário para começar!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Roboto',
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text(
+                  "Ok",
+                  style: TextStyle(
+                    color: Color(0xFF60B2A3),
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
